8b05979b8d6f0c3c6943f58408f96aa4dae60514
Mayank Surya <mayankkandk@gmail.com>
2024-10-14 20:45:35 -0400

    completed 90%, need to make it responsive

---
+++ Logo Shrink/index.js
@@ -0,0 +1,19 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const centerLogo = document.querySelector(".center");
+const imgContainer = document.querySelector(".img-container");
+const mainImg = imgContainer.querySelector(".main-img");
+
+const tl = gsap.timeline();
+
+// Animate the logo position and scale
+tl.from(centerLogo, {
+  top: "50vh",
+  scale: 2,
+  scrollTrigger: {
+    trigger: imgContainer,
+    markers: true,
+    start: "top",
+    scrub: true,
+  },
+});
