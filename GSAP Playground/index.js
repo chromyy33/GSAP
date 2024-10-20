48f11fc31f2bf9365e36d1d10c3d2b9a855f7f87
Mayank Surya <mayankkandk@gmail.com>
2024-10-19 22:15:57 -0400

    simple animation made and learnt about gsap getter and setter functions

--- GSAP Playground/index.js
+++ GSAP Playground/index.js
@@ -7,10 +7,61 @@
 
 //Stagger
 
-gsap.to(".box", {
-  y: -300,
-  stagger: {
-    each: 0.5,
-    from: "edges",
+// gsap.to(".box", {
+//   y: -300,
+//   stagger: {
+//     each: 0.5,
+//     from: "edges",
+//   },
+// });
+
+//make a tl animation using labels and relative timings
+
+const tl = gsap.timeline();
+
+tl.from([".box1, .box2, .box3"], {
+  x: -100,
+  duration: 1,
+  repeat: -1,
+  ease: "linear",
+  yoyo: true,
+}).to(
+  [".box7, .box6,.box5"],
+  {
+    x: 100,
+    duration: 1,
+    repeat: -1,
+    yoyo: true,
   },
-});
+  "<"
+);
+//herman
+// let select = (e) => document.querySelector(e);
+// let selectAll = (e) => document.querySelectorAll(e);
+
+// let progressSlider = select("#progressSlider");
+// let time = select("#time");
+// let pause = select("#pause"); //better than document.querySelector("#pause")
+
+// let animation = gsap.to("#herman", {
+//   duration: 6,
+//   ease: "none",
+//   motionPath: {
+//     path: "#motionpath",
+//     align: "#herman",
+//   },
+//   onComplete: () => {
+//     pause.innerText = "play";
+//   },
+// });
+// // animation.time(6);//jumps to the time in the args
+// // animation.duration(8)// sets duration of the animation to 8s
+// let isPaused = animation.paused();
+// pause.addEventListener("click", function () {
+//   if (animation.progress() === 1) {
+//     animation.restart();
+//   }
+//   pause.innerText = isPaused ? "pause" : "play";
+//   isPaused ? animation.play() : animation.pause();
+//   isPaused = !isPaused;
+// });
